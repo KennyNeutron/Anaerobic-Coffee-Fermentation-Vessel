f3989c68835afa3f8c99eded196646331a5e4805
unknown <kennyneutron.hex@gmail.com>
2023-12-23 21:38:30 +0800

    UPDATE: added m5, tds, & pH4052c sensors + surface temp fixed

--- Anaerobic-Coffee-Fermentation-Vessel.ino
+++ Anaerobic-Coffee-Fermentation-Vessel.ino
@@ -14,10 +14,35 @@ Date Finished:
 #include <Wire.h>
 #include "SHTSensor.h"
 #include <Adafruit_AHTX0.h>
+#include <EEPROM.h>
+#include "GravityTDS.h"
+#include <ph4502c_sensor.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define TdsSensorPin A0
+
+// Define the pH level pin and temperature pin
+#define PH4502C_PH_LEVEL_PIN A1
+#define PH4502C_TEMP_PIN A2
+
+#define ONE_WIRE_BUS 5
+
+
+// Create an instance of the PH4502C_Sensor
+PH4502C_Sensor ph4502(PH4502C_PH_LEVEL_PIN, PH4502C_TEMP_PIN);
+GravityTDS gravityTds;
+
+float tdsValue = 0;
 
 SHTSensor m5(SHTSensor::SHT3X);
 
 Adafruit_AHTX0 aht20;
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature dallas_sensors(&oneWire);
 
 void setup() {
   Serial.begin(115200);
@@ -33,12 +58,40 @@ void setup() {
     while (1) delay(10);
   }
   Serial.println("AHT10 or AHT20 found");
+
+  gravityTds.setPin(TdsSensorPin);
+  gravityTds.setAref(5.0);       //reference voltage on ADC, default 5.0V on  Arduino UNO
+  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+  gravityTds.begin();            //initialization
+
+  // Initialize the PH4502 instance
+  ph4502.init();
+
+  dallas_sensors.begin();  // Start up the library
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
   // read from first sensor
+  Serial.print("\n\n\n############################################\n\n\n");
+
+  Serial.println("================================================");
+  Serial.println("Surface Temperature Sensor:");
+  dallas_sensors.requestTemperatures();
+
+  //print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(dallas_sensors.getTempCByIndex(0));
+  Serial.print((char)176);  //shows degrees character
+  Serial.print("C  |  ");
+
+  //print the temperature in Fahrenheit
+  Serial.print((dallas_sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
+  Serial.print((char)176);  //shows degrees character
+  Serial.println("F");
+  Serial.println("================================================");
+
   if (m5.readSample()) {
     Serial.println("================================================");
     Serial.println("M5 Sensor:");
@@ -55,7 +108,7 @@ void loop() {
 
   sensors_event_t aht20_humidity, aht20_temp;
   aht20.getEvent(&aht20_humidity, &aht20_temp);  // populate temp and humidity objects with fresh data
-  Serial.println("\n\n");
+  Serial.print("\n\n\n");
   Serial.println("================================================");
   Serial.println("AHT20 Sensor:");
   Serial.print("Temperature: ");
@@ -65,6 +118,36 @@ void loop() {
   Serial.print(aht20_humidity.relative_humidity);
   Serial.println("% rH");
   Serial.println("================================================");
+
+  Serial.print("\n\n\n");
+  Serial.println("================================================");
+  Serial.println("TDS Sensor:");
+  gravityTds.setTemperature(dallas_sensors.getTempCByIndex(0));  // set the temperature and execute temperature compensation
+  gravityTds.update();                                //sample and calculate
+  tdsValue = gravityTds.getTdsValue();                // then get the value
+  Serial.print(tdsValue, 0);
+  Serial.println("ppm");
+  Serial.println("================================================");
+
+  Serial.print("\n\n\n");
+  Serial.println("================================================");
+  Serial.println("pH4052C Sensor:");
+  // Read the temperature from the sensor
+  //Serial.println("Temperature reading:"
+  //               + String(ph4502.read_temp()));
+
+  // Read the pH level by average
+  Serial.println("pH Level Reading: "
+                 + String(ph4502.read_ph_level()));
+
+  // Read a single pH level value
+  //Serial.println("pH Level Single Reading: "
+  //               + String(ph4502.read_ph_level_single()));
+  Serial.println("================================================");
+
+
+
+  Serial.print("\n\n\n############################################");
 
   delay(1000);
 }
