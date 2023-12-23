73b4aa0c016ca992c90aa2baac4b956713027d02
unknown <kennyneutron.hex@gmail.com>
2023-12-23 11:52:15 +0800

    UPDATE: added aht20 sensor

--- Anaerobic-Coffee-Fermentation-Vessel.ino
+++ Anaerobic-Coffee-Fermentation-Vessel.ino
@@ -13,37 +13,58 @@ Date Finished:
 */
 #include <Wire.h>
 #include "SHTSensor.h"
+#include <Adafruit_AHTX0.h>
 
 SHTSensor m5(SHTSensor::SHT3X);
 
+Adafruit_AHTX0 aht20;
 
-
-void setup(){
-    Serial.begin(115200);
-    Serial.println("\nAnaerobic Coffee Fermentation Vessel \n\n\nSYSTEM STARTING...");
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nAnaerobic Coffee Fermentation Vessel \n\n\nSYSTEM STARTING...");
 
   Wire.begin();
 
   // initialize sensor with normal i2c-address
   m5.init();
+
+  if (!aht20.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+    while (1) delay(10);
+  }
+  Serial.println("AHT10 or AHT20 found");
 }
 
 
-void loop(){
+void loop() {
   // put your main code here, to run repeatedly:
   // read from first sensor
   if (m5.readSample()) {
-    Serial.print("SHT1 :\n");
+    Serial.println("================================================");
+    Serial.println("M5 Sensor:");
     Serial.print("  RH: ");
-    Serial.print(m5.getHumidity(), 2);
-    Serial.print("\n");
+    Serial.println(m5.getHumidity(), 2);
     Serial.print("  T:  ");
-    Serial.print(m5.getTemperature(), 2);
-    Serial.print("\n");
+    Serial.println(m5.getTemperature(), 2);
+    Serial.println("================================================");
   } else {
-    Serial.print("Sensor 1: Error in readSample()\n");
+    Serial.println("================================================");
+    Serial.println("M5 Sensor: Error in readSample()");
+    Serial.println("================================================");
   }
 
+  sensors_event_t aht20_humidity, aht20_temp;
+  aht20.getEvent(&aht20_humidity, &aht20_temp);  // populate temp and humidity objects with fresh data
+  Serial.println("\n\n");
+  Serial.println("================================================");
+  Serial.println("AHT20 Sensor:");
+  Serial.print("Temperature: ");
+  Serial.print(aht20_temp.temperature);
+  Serial.println(" C");
+  Serial.print("Humidity: ");
+  Serial.print(aht20_humidity.relative_humidity);
+  Serial.println("% rH");
+  Serial.println("================================================");
 
   delay(1000);
 }
